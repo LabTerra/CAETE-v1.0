e69b64b899cdbb84b79cb20cf748141f6186badb
jpdarela <jpdarela@rc.unesp.br>
2018-02-24 15:09:23 -0300

    mp for windows

--- aux_files/homedir.py
+++ aux_files/homedir.py
@@ -2,13 +2,15 @@
    """
 
 import os
-
-
 # CHANGE HOMEDIR to make tests in forks
 # for example: to run in sombrero make HOMEDIR = os.sep.join(['','home','amazonfaceme','your username'])
 
+
 py_executable = 'ipython3'
 HOMEDIR = os.sep.join(['', 'home', 'jpdarela', 'Desktop'])
+RESULTS_DIR = os.sep.join([HOMEDIR, 'results'])
+TMP_DIR = os.sep.join([HOMEDIR, 'tmp'])
+OUTPUT_NC_DIR = os.sep.join([TMP_DIR, 'outputs_nc'])
 
 # EDIT AND SAVE IN your /src folder (in your fork)
 # py_executable = 'python'
