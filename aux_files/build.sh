5d6c77d05d338c3ba6a94e7c8644783b3eb27b5c
João Paulo Darela Filho <jpdarela@rc.unesp.br>
2018-12-10 18:54:10 -0200

    correct build

--- aux_files/build.sh
+++ aux_files/build.sh
@@ -4,8 +4,9 @@
 ./clean_dir.sh
 
 ## Maybe you need to change the python executable  accortding your system
-python -m numpy.f2py -h caete_module.pyf global.f90 funcs.f90 productivity.f90$
-python -m numpy.f2py caete_module.pyf -c global.f90 funcs.f90 productivity.f90$
+python -m numpy.f2py -h caete_module.pyf global.f90 funcs.f90 productivity.f90 budget.f90 water_balance.f90 -m caete_module --overwrite-signature
+python -m numpy.f2py caete_module.pyf -c global.f90 funcs.f90 productivity.f90 budget.f90 water_balance.f90
+
 
 
 
